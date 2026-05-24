8f8b7a4cb1426e7ac96dd0f69e8a2464b2b28ec6
allao404 <allamistakeo@163.com>
2026-05-24 20:10:14 +0800

    Update IndexController.kt

--- djl-spring-boot-web/src/main/kotlin/com/aws/samples/djldemoweb/controller/IndexController.kt
+++ djl-spring-boot-web/src/main/kotlin/com/aws/samples/djldemoweb/controller/IndexController.kt
@@ -10,19 +10,28 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
+// 网页控制器包路径
 package com.aws.samples.djldemoweb.controller
 
+// 引入网页控制器注解
 import org.springframework.stereotype.Controller
+// 引入请求路径映射注解
 import org.springframework.web.bind.annotation.RequestMapping
 
 /**
- * Index controller.
+ * 首页访问控制器
+ * 处理网站根路径请求，跳转首页页面
  */
 @Controller
 class IndexController {
 
+    /**
+     * 根路径请求映射
+     * @return 首页视图页面名称
+     */
     @RequestMapping("/")
     fun index(): String {
+        // 返回首页模板视图
         return "index"
     }
 }
