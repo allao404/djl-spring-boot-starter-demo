d30dd2aefe1e9b69c5fde278dc9f3a5e519f554d
allao404 <allamistakeo@163.com>
2026-05-24 20:13:08 +0800

    Update ObjectDetectionForm.kt

--- djl-spring-boot-web/src/main/kotlin/com/aws/samples/djldemoweb/form/ObjectDetectionForm.kt
+++ djl-spring-boot-web/src/main/kotlin/com/aws/samples/djldemoweb/form/ObjectDetectionForm.kt
@@ -10,12 +10,20 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
+// 表单实体类存放包
 package com.aws.samples.djldemoweb.form
 
+// 非空校验注解
 import org.jetbrains.annotations.NotNull
+// 接收前端上传的文件对象
 import org.springframework.web.multipart.MultipartFile
 
+/**
+ * 物体检测提交表单实体
+ * 用于绑定前端上传的图片文件数据
+ */
 class ObjectDetectionForm {
+    // 上传的图片文件，非空必填项
     @NotNull
     var file: MultipartFile? = null
 }
